a6fe2a95b1e72dfc7baa5d9791539ef4bd3158e7
TEKITER-PC <tekiter8@gmail.com>
2019-08-13 02:55:02 +0900

    Add scoped property

--- hydor_project/src/components/BoardList.vue
+++ hydor_project/src/components/BoardList.vue
@@ -37,7 +37,7 @@ export default{
     }
 }
 </script>
-<style>
+<style scoped>
 .header{background-color:aqua; padding: 10px 0px 0px 0px;}
 .headerText{padding:0px 20px 0px 20px;}
 ul{list-style-type:none;margin:0; padding:0;
