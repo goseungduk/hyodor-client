eb3e499dd6ee9a663b6a69efef0d55fabd5528da
goseungduk <a457581@catholic.ac.kr>
2019-08-13 20:40:29 +0900

    help

--- hydor_project/src/router/BoardList.vue
+++ hydor_project/src/router/BoardList.vue
@@ -1,30 +1,4 @@
 <template>
-<div>
-    <div id="app" ref="app">
-    <slider :width="280" format="overlay" direction="left" :opacity="0.15"
-    :links="[{'id':1,'text':'방문,전화 기록일기','url':'#'},
-    {'id':2,'text':'복지 정보','url':'#'},
-    {'id':3,'text':'게시판','url':'http://localhost:8080/boardlist'},
-    {'id':4,'text':'마이페이지','url':'#'}
-    ] "></slider> 
-    <!-- https://github.com/jeremyhamm/vue-slider -->
-    <!-- url href로 안하고 vue-router로 하면 깔끔한데 
-        소스 수정을 해야함. 수정은 간단함. -->
-    <!-- 근데 Github에서 어떻게 온전히 가져오지...? -->
-  </div>
-<div class="header">
-    <nav>
-      <ul>
-        <li><router-link v-bind:to="{name:'free'}">자유게시판</router-link></li>
-        <li><router-link v-bind:to="{name:'gift'}">선물 추천</router-link></li>
-        <li><router-link v-bind:to="{name:'gomin'}">고민 상담</router-link></li>
-        <li><router-link v-bind:to="{name:'letter'}">편지 쓰기</router-link></li>
-        <li><router-link v-bind:to="{name:'partyPlace'}">경조사 장소 알려주세요!</router-link></li>
-      </ul>
-    </nav>
-</div>
-  <div class="container">
-=======
   <div>
     <b-nav current-page="boardlist"></b-nav>
     <div id="app" ref="app"></div>
