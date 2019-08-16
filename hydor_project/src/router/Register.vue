f7df88c5c206d49eb24cd6227ff58cb8aba1aa8b
TEKITER-PC <tekiter8@gmail.com>
2019-08-16 16:22:26 +0900

    Add form validation

--- hydor_project/src/router/Register.vue
+++ hydor_project/src/router/Register.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <b-container class="mainscreen mt-5">
+    <b-container class="mt-5">
       <h2>효도르 회원가입</h2>
       <div>
         <b-form @submit="onSubmit">
           <b-form-group>
-            <b-form-input v-model="form.username" placeholder="아이디" required></b-form-input>
+            <b-form-input v-model="form.username" placeholder="아이디" :state="isValid(errormsg.username)"></b-form-input>
+            <b-form-invalid-feedback :state="isValid(errormsg.username)">{{ errormsg.username }}</b-form-invalid-feedback>
+            
           </b-form-group>
           <b-form-group>
-            <b-form-input v-model="form.password" type="password" placeholder="비밀번호" required></b-form-input>
+            <b-form-input v-model="form.password" type="password" placeholder="비밀번호" :state="isValid(errormsg.password)"></b-form-input>
+            <b-form-invalid-feedback :state="isValid(errormsg.password)">{{ errormsg.password }}</b-form-invalid-feedback>
           </b-form-group>
           <b-form-group>
-            <b-form-input v-model="form.email" type="email" placeholder="이메일" required></b-form-input>
+            <b-form-input v-model="form.email" type="email" placeholder="이메일" :state="isValid(errormsg.email)"></b-form-input>
+            <b-form-invalid-feedback :state="isValid(errormsg.email)">{{ errormsg.email }}</b-form-invalid-feedback>
           </b-form-group>
           <b-form-group>
-            <b-form-input v-model="form.nickname" placeholder="닉네임" required></b-form-input>
+            <b-form-input v-model="form.nickname" placeholder="닉네임" :state="isValid(errormsg.nickname)"></b-form-input>
+            <b-form-invalid-feedback :state="isValid(errormsg.nickname)">{{ errormsg.nickname }}</b-form-invalid-feedback>
           </b-form-group>
           <b-button type="submit" variant="primary">회원가입</b-button>
         </b-form>
@@ -23,14 +28,20 @@
   </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 import * as session from "../utils/loginService.js";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       form: {
+        username: "",
+        password: "",
+        email: "",
+        nickname: ""
+      },
+      errormsg: {
         username: "",
         password: "",
         email: "",
@@ -39,11 +50,9 @@ export default {
     };
   },
   computed: {
-
-  },
-  components: {
     
   },
+  components: {},
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
@@ -53,32 +62,73 @@ export default {
         email: this.form.email,
         nickname: this.form.nickname
       };
-      
-      axios.put(session.apiurl + "login", payloads)
-      .then(function (response) {
-        alert("회원가입이 완료되었습니다. 다시 로그인해 주세요.");
-        this.$router.push({ name: "login" });
-      })
-      .catch(function (error) {
-        if (error.response) {
-          if (error.response.status == 409) {
-            alert("중복된 아이디 또는 비밀번호");
-            return;
+
+      this.errormsg.username = "";
+      this.errormsg.password = "";
+      this.errormsg.email = "";
+      this.errormsg.nickname = "";
+
+      var nthis = this;
+
+      axios
+        .put(session.apiurl + "login", payloads)
+        .then(function(response) {
+          alert("회원가입이 완료되었습니다. 다시 로그인해 주세요.");
+          nthis.$router.push({ name: "login" });
+        })
+        .catch(function(error) {
+          if (error.response) {
+            if (error.response.status == 409 || error.response.status == 400) {
+              let errdat = error.response.data;
+              if (errdat['username']) {
+                nthis.errormsg.username = errdat['username'];
+              }
+              if (errdat['password']) {
+                nthis.errormsg.password = errdat['password'];
+              }
+              if (errdat['email']) {
+                nthis.errormsg.email = errdat['email'];
+              }
+              if (errdat['nickname']) {
+                nthis.errormsg.nickname = errdat['nickname'];
+              }
+
+              return;
+            }
           }
-          else if (error.response.status == 400) {
-            alert("잘못된 입력값");
-            return;
-          }
-        }
-        alert("알 수 없는 오류");
-      })
+          alert("알 수 없는 오류");
+        });
+    },
+
+    isValid(errmsg) {
+      if (errmsg == "") {
+        return null;
+      }
+      return false;
     }
   }
 };
 </script>
 <style scoped>
 
+/* PC */
+@media (min-width: 1200px) { 
+    .container{
+        max-width: 400px;
+    }
+}
 
-.mainscreen {
+/* PC and Tablet */
+@media (min-width: 992px) {
+    .container{
+        max-width: 400px;
+    }
+}
+
+/* Tablet */
+@media (min-width: 768px) {
+    .container{
+        max-width: 400px;
+    }
 }
 </style>
