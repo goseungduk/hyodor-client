17e43dce35508733bade637aa60daeb17df5a0eb
goseungduk <a457581@catholic.ac.kr>
2019-08-18 02:12:57 +0900

    everytimeUpdate

--- hydor_project/src/router/FreeBoard.vue
+++ hydor_project/src/router/FreeBoard.vue
@@ -1,99 +1,102 @@
 <template>
   <div>
     <div id="example">
-      <b-table
-        striped
-        hover
-        :fields="fields"
-        :items="items"
-        :per-page="perPage"
-        @row-clicked="logging"
-        :current-page="currentPage"
-      ></b-table>
-      <b-pagination
-        v-model="currentPage"
-        :total-rows="rows"
-        :per-page="perPage"
-        aria-controls="my-table"
-      ></b-pagination>
+      <article v-for="i in paginatedData" :key="i.id">
+        <a href="#">
+          <h2>{{i.title}}</h2>
+          <p>{{i.content}}</p>
+          <h3>{{i.writer.nickname}}</h3>
+        </a>
+      </article>
+      <div class="btn-cover">
+        <button :disabled="pageNum === 0" @click="prevPage" class="page-btn">이전</button>
+        <span class="page-count">{{ pageNum + 1 }} / {{ pageCount }} 페이지</span>
+        <button :disabled="pageNum >= pageCount - 1" @click="nextPage" class="page-btn">다음</button>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import Constant from "../Constant";
-import { mapState } from "vuex";
 import axios from "axios";
 import * as session from "../utils/loginService.js";
 export default {
   name: "freeboard",
-  props: ["no"],
+  props: {
+    no: { type: Number }, // 게시판 번호 받아오는 props
+    pageSize: { // 한 페이지당 보여줄 게시물 개수
+      type: Number,
+      required: false,
+      default: 5  // 디폴트 값은 5개
+    }
+  },
   data: function() {
     return {
-      perPage: 7,
-      currentPage: 1,
-      fields: [
-        {
-          key: "id",
-          label: "번호"
-        },
-        {
-          key: "title",
-          label: "제목",
-          colType: "a href"
-        },
-        {
-          key: "writer.nickname",
-          label: "작성자"
-        },
-        {
-          key: "vote_up",
-          label: "추천"
-        }
-      ],
-      items: []
+      items: [], // 게시판 post 모아두는 변수
+      pageNum: 0, // 기본 pageNumber
+      length: "" 
+      // 전체 post 개수 , totalcount가 아닌 count 값(현재 삭제되지않고 유지되고 있는 게시물)을 가져옴
     };
   },
   methods: {
-    logging(record) {
-      console.log(record);
+    nextPage() { // 다음 페이지로 가는 함수
+      this.pageNum += 1;
     },
+    prevPage() { // 이전 페이지로 가는 함수
+      this.pageNum -= 1;
+    }
   },
   computed: {
-    // ...mapState(['post']),
-    rows() {
-      return this.items.length;
+    pageCount() { // 게시물 개수는 변화가 있을 때마다 실시간으로 compute 해준다
+      let listLeng = this.length;
+      let listSize = this.pageSize;
+      let page = Math.floor(listLeng / listSize);
+      if (listLeng % listSize > 0) page += 1;
+
+      /*
+      아니면 page = Math.floor((listLeng - 1) / listSize) + 1;
+      이런식으로 if 문 없이 고칠 수도 있다!
+      */
+      return page;
+    },
+    paginatedData() { // 전체 게시물에서 pageSize 만큼 slicing된 posts
+      const start = this.pageNum * this.pageSize,
+        end = start + this.pageSize;
+      return this.items.slice(start, end);
     }
   },
   mounted: function() {
-    axios.get("http://203.229.206.16:12345/api/v1/" + "board/" + this.no)
+    axios
+      .get("http://203.229.206.16:12345/api/v1/" + "board/" + this.no)
       .then(response => {
         this.items = response.data.posts;
+        this.length = response.data.count;
+        // this.itemss=response.data.posts;
       })
       .catch(e => {
         alert("서버오류!" + e);
         this.items = [];
       });
   },
-  watch:{ //즉각적으로 게시물 업뎃 하기위한 watch
-      no:function(){
-          axios.get("http://203.229.206.16:12345/api/v1/" + "board/" + this.no)
-      .then(response => {
-        this.items = response.data.posts;
-      })
-      .catch(e => {
-        alert("서버오류!" + e);
-        this.items = [];
-      });
-      }
+  watch: {
+    // 게시판 번호의 변화에 따라 즉각적으로 게시물 업뎃 하기위한 watch
+    no: function() {
+      this.pageNum=0; 
+      this.pageSize=5;
+      // 게시판이 바뀔때마다 첫페이지로 바꿔주자
+      axios
+        .get("http://203.229.206.16:12345/api/v1/" + "board/" + this.no)
+        .then(response => {
+          this.items = response.data.posts;
+          this.length = response.data.count;
+          // this.itemss=response.data.posts;
+        })
+        .catch(e => {
+          alert("서버오류!" + e);
+          this.items = [];
+        });
+    }
   }
-  // created:function(){
-  //     console.log(this.no);
-  //     this.$store.dispatch(Constant.FETCH_POSTS,{no:1});
-  //     //일반 정수값을 no에 전달해주면 바로바로 게시글 로드가 되는데
-  //     //props의 no를 가져다 전달해주면 늦음
-  //     //>??
-
-  // }
 };
 </script>
 <style scoped>
