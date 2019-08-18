2ba942ffe2ba7e584f7b9aefb8642f7428d399d1
goseungduk <a457581@catholic.ac.kr>
2019-08-19 02:07:24 +0900

    댓글대충디자인

--- hydor_project/src/router/ViewBoard.vue
+++ hydor_project/src/router/ViewBoard.vue
@@ -1,32 +1,28 @@
 <template>
     <div>
         <div>
-        
             <b-card :title="items.title" :sub-title="items.writer.nickname">
-        
                 <b-card-text>
-        
                     {{items.content}}
-        
                 </b-card-text>
-        
             </b-card>
-        
         </div>
         <span> 
             <!-- 텍스트 박스랑 작성 버튼이 한 줄에 들어가게 부탁드립니당 -->
             <b-form-input v-model="comment" placeholder="댓글을 입력하세요"></b-form-input>
             <b-button variant="outline-primary" @click="commenting()">작성</b-button>
         </span>
-        <div>
-            <!-- 게시물 이나 댓글창에서 에브리타임 사람 아이콘 처럼 사진 비춰주는거 고려해봐도 괜찮을 것 같습니다 -->
-            <article v-for="i in items.comments" :key="i.id">
-                <h5>{{i.writer.nickname}}</h5>
-                <p>{{i.content}}</p>
-                <ul>
-                    <li>추천수 : {{i.vote_up}}</li>
-                </ul>
-            </article>
+        <div class="mother">
+            <div class="comments" style="display: block;">
+                <!-- 게시물 이나 댓글창에서 에브리타임 사람 아이콘 처럼 사진 비춰주는거 고려해봐도 괜찮을 것 같습니다 -->
+                <article v-for="i in items.comments" :key="i.id" class="parent">
+                    <h5>{{i.writer.nickname}}</h5>
+                    <p>{{i.content}}</p>
+                    <ul class="status commentvotestatus">
+                        <li class="vote commentvote" style="display: list-item;">추천수 : {{i.vote_up}}</li>
+                    </ul>
+                </article>
+            </div>
         </div>
     </div>
 </template>
@@ -104,5 +100,50 @@ export default {
 }
 </script>
 <style scoped>
-
+#container > div.articles > article ul.status li {
+    float: left;
+    margin-left: 8px;
+    padding: 0 2px;
+    height: 20px;
+    line-height: 20px;
+    color: #a6a6a6;
+    font-size: 12px;
+    background-repeat: no-repeat;
+    background-position: left center;
+    background-size: 12px 12px;
+    cursor: pointer;
+}
+#container > div.articles > article > div.comments article ul.commentvotestatus {
+    float: left;
+}
+#container > div.articles > article ul.status li.vote {
+    padding-left: 15px;
+    color: #c62917;
+}
+dl, ul, ol, menu, li {
+    list-style: none;
+}
+element.style {
+    display: list-item;
+}
+div.mother{
+    border:1px solid gray;
+}
+article.parent {
+    padding: 15px 15px 0 15px;
+    border-top: 1px solid black;
+}
+#container > div.articles > article > div.comments {
+    display: none;
+}
+element.style {
+    display: block;
+}
+body, div, nav, aside, article, h1, h2, h3, h4, ol, ul, li, dl, dt, dd, p, span, form, th, td, input, textarea, select, pre, address {
+    color: #666;
+    font-family: "맑은 고딕",  tahoma;
+    _font-family: 돋움, tahoma;
+    font-size: 12px;
+    letter-spacing: -1px;
+}
 </style>
