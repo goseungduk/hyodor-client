187f82f667568aec6918c6cfacdd2243ded36d22
TEKITER-PC <tekiter8@gmail.com>
2019-08-20 16:59:59 +0900

    Change navbar structure

--- hydor_project/src/components/Navbar.vue
+++ hydor_project/src/components/Navbar.vue
@@ -1,36 +1,41 @@
 <template>
-    
-    <div>
-        <b-navbar toggleable="lg" type="light" variant="light">
-          <b-nav>
-            <b-nav-item href="/home" class="hydorlogo"><img onclick="location.href='/home'" class="mr-2" src="../assets/logo2.png" width="40" height="30" /></b-nav-item>
-          </b-nav>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand class="hyodor" href="/home">
+        <img src="../assets/logo2.png" class="hyodorlogo mr-1 align-top" />
+        Hyodor
+      </b-navbar-brand>
 
-          <b-navbar-brand  class="hyodor" href="/home">Hyodor</b-navbar-brand>
-
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="/home" :active="isactive('home')" :class="[isactive('home') ? 'n_active':'non_active']" >일정</b-nav-item>
-          <b-nav-item href="/boardlist" :active="isactive('boardlist')" :class="[isactive('boardlist') ? 'n_active':'non_active']" >커뮤니티</b-nav-item>
-          <b-nav-item href="/serviceinfo" :active="isactive('serviceinfo')" :class="[isactive('serviceinfo') ? 'n_active':'non_active']" >복지정보(demo)</b-nav-item>  
+          <b-nav-item
+            href="/home"
+            :active="isactive('home')"
+            :class="[isactive('home') ? 'n_active':'non_active']"
+          >일정</b-nav-item>
+          <b-nav-item
+            href="/boardlist"
+            :active="isactive('boardlist')"
+            :class="[isactive('boardlist') ? 'n_active':'non_active']"
+          >커뮤니티</b-nav-item>
+          <b-nav-item
+            href="/serviceinfo"
+            :active="isactive('serviceinfo')"
+            :class="[isactive('serviceinfo') ? 'n_active':'non_active']"
+          >복지정보(demo)</b-nav-item>
         </b-navbar-nav>
 
         <b-navbar-nav class="ml-auto">
-          
-
-          
-
           <b-nav-item-dropdown :text="username" right>
-            
             <b-dropdown-item href="#">사용자 정보</b-dropdown-item>
             <b-dropdown-item @click="logout()">로그아웃</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    </div>
+  </div>
 </template>
 
 
@@ -39,23 +44,19 @@ import { mapState } from "vuex";
 import * as session from "../utils/loginService.js";
 
 export default {
-  name: 'hNav',
-  components: {
-    
-  },
-  props: ['currentPage'],
+  name: "hNav",
+  components: {},
+  props: ["currentPage"],
   computed: {
     ...mapState(["account"]),
     username: () => {
       // return "Username"
       return session.getNickname();
-    },
-
+    }
   },
   methods: {
     isactive(route) {
       if (this.currentPage == route) {
-        
         return true;
       }
       return false;
@@ -70,56 +71,60 @@ export default {
 
 
 <style scoped>
-.hydorlogo{
-  border:1px solid #f8f9fa;
+.hyodorlogo {
+  height: 1.4em;
 }
-.hydorlogo:hover{
-  background-color: #f1f1f1;
+.n_active {
+  float: left;
 }
-.n_active{
-float:left;
-}
-.n_active { font-weight:bold; 
- text-decoration:none; border-bottom: 2px solid #026CAD;
+.n_active {
+  font-weight: bold;
+  text-decoration: none;
+  border-bottom: 2px solid #026cad;
 }
 .navbar .navbar-nav .nav-link {
-  color: #026CAD!important
+  color: #026cad !important;
 }
-.non_active{
-  color:#3764CC; font-weight:bold;
-}
-.non_active:hover{
+.non_active {
+  color: #3764cc;
   font-weight: bold;
-  border-bottom: 2px solid #026CAD;
 }
-.hyodor:hover{
-  color:#026CAD;
-}
-@media(max-width: 720px){
-.n_active { font-weight:bold; 
- background-color: #EDF2F9;
-}
-.non_active:hover{
+.non_active:hover {
   font-weight: bold;
-  background-color: #EDF2F9;
+  border-bottom: 2px solid #026cad;
 }
-} 
-@media(min-width: 720px)and (max-width:990px){
-.n_active { font-weight:bold; 
- background-color: #EDF2F9;
+.hyodor:hover {
+  color: #026cad;
 }
-.non_active:hover{
-  font-weight: bold;
-  background-color: #EDF2F9;
+@media (max-width: 720px) {
+  .n_active {
+    font-weight: bold;
+    background-color: #edf2f9;
+  }
+  .non_active:hover {
+    font-weight: bold;
+    background-color: #edf2f9;
+  }
 }
-} 
-@media(min-width: 990px){
-.n_active { font-weight:bold; 
- text-decoration:none; border-bottom: 2px solid #026CAD;
-} 
-.non_active:hover{
-  font-weight: bold;
-  border-bottom: 2px solid #026CAD;
+@media (min-width: 720px) and (max-width: 990px) {
+  .n_active {
+    font-weight: bold;
+    background-color: #edf2f9;
+  }
+  .non_active:hover {
+    font-weight: bold;
+    background-color: #edf2f9;
+  }
 }
+@media (min-width: 990px) {
+  .n_active {
+    font-weight: bold;
+    text-decoration: none;
+    border-bottom: 2px solid #026cad;
+  }
+  .non_active:hover {
+    font-weight: bold;
+    border-bottom: 2px solid #026cad;
+  }
 }
 </style>
