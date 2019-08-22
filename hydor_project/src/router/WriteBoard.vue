2cb63f1d2e46583a53ff7cd2701ca25591828256
임소진 <pandan4592@gmail.com>
2019-08-22 07:28:20 +0000

    Add layout

--- hydor_project/src/router/WriteBoard.vue
+++ hydor_project/src/router/WriteBoard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div style="box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.1);padding: 50px;margin-bottom: 30px;">
        <b-form-input class="mt-2" v-model="title" placeholder="제목을 입력하세요"></b-form-input>
         <b-form-textarea class="mt-2" id="textarea" v-model="content" placeholder="내용을 입력하세요" rows="8" max-rows="8"></b-form-textarea>
         <b-button class="mt-1 button"  @click="submit()">작성하기</b-button>
